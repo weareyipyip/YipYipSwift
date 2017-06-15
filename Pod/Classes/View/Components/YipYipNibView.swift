c8787d90709a3f0425a7f0b646bc8763d6d2760f
Rens Wijnmalen <r.wijnmalen@yipyip.nl>
2017-06-15 09:01:52 +0200

    Added YipYipNibView

---
+++ Pod/Classes/View/Components/YipYipNibView.swift
@@ -0,0 +1,36 @@
+//
+//  NibView.swift
+//  Commando Training
+//
+//  Created by Rens Wijnmalen on 15/06/2017.
+//  Copyright © 2017 YipYip. All rights reserved.
+//
+
+import UIKit
+
+class NibView: UIView {
+    
+    var nibView:UIView!
+    var nibFileName:String? {
+        return nil
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.nibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.nibSetup()
+    }
+    
+    func nibSetup() {
+        if let nibFileName = self.nibFileName{
+            self.nibView = YipYipUtils.view.loadViewFromNib(name: nibFileName, toView: self)
+        } else {
+            print("NibView: Property \"nibFileName\" must be override")
+        }
+    }
+
+}
--- Pod/Classes/View/ViewUtils.swift
+++ Pod/Classes/View/ViewUtils.swift
@@ -315,4 +315,27 @@ open class ViewUtils
         
         return height
     }
+    
+    // -----------------------------------------------------------
+    // -- Nib view handling
+    // -----------------------------------------------------------
+    
+    open func loadViewFromNib(name:String, toView view:UIView)->UIView {
+        
+        let bundle = Bundle(for: type(of: self))
+        let views = UINib(nibName: name, bundle: bundle).instantiate(withOwner: self, options: nil) as [AnyObject]
+        var returnView = UIView()
+        if let view = views[0] as? UIView{
+            returnView = view
+        } else {
+            returnView = UIView()
+        }
+        
+        returnView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(returnView)
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[view]|", options: [], metrics: nil, views: ["view":returnView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|", options: [], metrics: nil, views: ["view":returnView]))
+        
+        return returnView
+    }
 }
