89bd3164c13bc5a992533adf13ea52883f068cb2
Marcel Bloemendaal <m.bloemendaal@yipyip.nl>
2018-06-04 10:31:02 +0200

    Slight improvements to YipYipNibButton and YipYipNibView

--- Pod/Classes/View/Components/YipYipNibButton.swift
+++ Pod/Classes/View/Components/YipYipNibButton.swift
@@ -11,12 +11,13 @@ import UIKit
 class YipYipNibButton: UIButton {
     
     public var nibView:UIView!
-    open var nibFileName:String? {
-        return nil
-    }
-    open var bundle:Bundle {
-        return Bundle.main
-    }
+
+    
+    // -----------------------------------------------------------------------------------------------------------------------
+    //
+    // MARK: - Initializers
+    //
+    // -----------------------------------------------------------------------------------------------------------------------
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,50 +29,50 @@ class YipYipNibButton: UIButton {
         self.nibSetup()
     }
     
-    fileprivate func nibSetup() {
-        let className = String(describing: type(of: self))
-        if let nibFileName = self.nibFileName{
-            if self.nibExist(name: nibFileName){
-                self.nibView = self.loadViewFromNib(name: nibFileName, toView: self)
-            } else {
-                print("NibView: File does nog exist")
-            }
-        }
-        else if self.nibExist(name: className){
-            self.nibView = self.loadViewFromNib(name: className, toView: self)
-        }
-        else {
-            print("NibView: Property \"nibFileName\" must be override")
+
+    // -----------------------------------------------------------------------------------------------------------------------
+    //
+    // MARK: - Computed properties
+    //
+    // -----------------------------------------------------------------------------------------------------------------------
+    
+    open var nibFileName:String? {
+        return nil
+    }
+    
+    open var bundle:Bundle {
+        return Bundle.main
+    }
+    
+    
+    // -----------------------------------------------------------------------------------------------------------------------
+    //
+    // MARK: - Public methods
+    //
+    // -----------------------------------------------------------------------------------------------------------------------
+    
+    // -----------------------------------------------------------
+    // MARK: -- View life cycle
+    // -----------------------------------------------------------
+
+    open func viewDidLayout(){
+    }
+    
+
+    // -----------------------------------------------------------------------------------------------------------------------
+    //
+    // MARK: - Private methods
+    //
+    // -----------------------------------------------------------------------------------------------------------------------
+    
+    private func nibSetup() {
+        let nibFileName:String = self.nibFileName == nil ? String(describing: type(of: self)) : self.nibFileName!
+        if YipYipUtils.view.nibExists(name: nibFileName, bundle: self.bundle) {
+            self.nibView = YipYipUtils.view.addViewFromNib(nibName: nibFileName, bundle: self.bundle, toOwner: self)
+        } else {
+            print("NibView: File does not exist")
         }
         self.nibView.isUserInteractionEnabled = false
         self.viewDidLayout()
-    }
-    
-    fileprivate func nibExist(name:String)->Bool{
-        if let _ = self.bundle.path(forResource: name, ofType: "nib"){
-            return true
-        }
-        return false
-    }
-    
-    fileprivate func loadViewFromNib(name:String, toView view:UIView)->UIView {
-        
-        let views = UINib(nibName: name, bundle: self.bundle).instantiate(withOwner: self, options: nil) as [AnyObject]
-        var returnView = UIView()
-        if let view = views[0] as? UIView{
-            returnView = view
-        } else {
-            returnView = UIView()
-        }
-        
-        returnView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(returnView)
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[view]|", options: [], metrics: nil, views: ["view":returnView]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|", options: [], metrics: nil, views: ["view":returnView]))
-        
-        return returnView
-    }
-    
-    open func viewDidLayout(){
     }
 }
