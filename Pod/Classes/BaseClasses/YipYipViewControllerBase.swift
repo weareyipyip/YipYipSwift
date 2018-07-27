80d85c16cf1f0c3e97ee96d391c19d6b4e5fff00
Rens Wijnmalen <r.wijnmalen@yipyip.nl>
2018-07-27 11:27:06 +0200

    Changed NSNotification to Notificatn

--- Pod/Classes/BaseClasses/YipYipViewControllerBase.swift
+++ Pod/Classes/BaseClasses/YipYipViewControllerBase.swift
@@ -10,18 +10,18 @@ import UIKit
 
 open class YipYipViewControllerBase: UIViewController {
     
-    private var _keyboardIsShown:Bool = false
-    private var _lastKnownKeyboardHeight:CGFloat = 0
+    private var _keyboardIsShown: Bool = false
+    private var _lastKnownKeyboardHeight: CGFloat = 0
     
     // ----------------------------------------------------
     // MARK: - Computed properties
     // ----------------------------------------------------
     
-    public var keyboardIsShown:Bool{
+    public var keyboardIsShown: Bool{
         return self._keyboardIsShown
     }
     
-    public var lastKnownKeyboardHeight:CGFloat{
+    public var lastKnownKeyboardHeight: CGFloat{
         return self._lastKnownKeyboardHeight
     }
     
@@ -32,9 +32,9 @@ open class YipYipViewControllerBase: UIViewController {
     override open func viewDidLoad() {
         super.viewDidLoad()
         
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillEnterForground(_:)), name: NSNotification.Name.UIApplicationWillEnterForeground, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillEnterForground(_:)), name: Notification.Name.UIApplicationWillEnterForeground, object: nil)
     }
     
     // ----------------------------------------------------
@@ -45,7 +45,7 @@ open class YipYipViewControllerBase: UIViewController {
         self.view.endEditing(true)
     }
     
-    @objc private func keyboardWillShow(_ notification:NSNotification){
+    @objc private func keyboardWillShow(_ notification: Notification){
         
         if self._keyboardIsShown{
             self.keyboardWillChange(notification: notification)
@@ -68,7 +68,7 @@ open class YipYipViewControllerBase: UIViewController {
         } else { return }
     }
     
-    @objc private func keyboardWillHide(_ notification:NSNotification){
+    @objc private func keyboardWillHide(_ notification: Notification){
         self.keyboardWillHide(notification: notification)
         
         guard let keyboardEndFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect else { return }
@@ -78,29 +78,29 @@ open class YipYipViewControllerBase: UIViewController {
         self._keyboardIsShown = false
     }
     
-    @objc private func applicationWillEnterForground(_ notification:NSNotification){
+    @objc private func applicationWillEnterForground(_ notification: Notification){
         self.dismissKeyboard()
     }
     
-    open func keyboardWillEnter(keyboardEndFrame:CGRect, animationDuration: TimeInterval, animationCurve: Int){
+    open func keyboardWillEnter(keyboardEndFrame: CGRect, animationDuration: TimeInterval, animationCurve: Int){
     }
     
-    open func keyboardWillEnter(notification:NSNotification){
+    open func keyboardWillEnter(notification: Notification){
     }
     
-    open func keyboardWillChange(keyboardEndFrame:CGRect, animationDuration: TimeInterval, animationCurve: Int){
+    open func keyboardWillChange(keyboardEndFrame: CGRect, animationDuration: TimeInterval, animationCurve: Int){
         // keyboardWillEnter will be called by default. If other behavour is required override this method in your controller and do not call the super method.
         self.keyboardWillEnter(keyboardEndFrame: keyboardEndFrame, animationDuration: animationDuration, animationCurve: animationCurve)
     }
     
-    open func keyboardWillChange(notification:NSNotification){
+    open func keyboardWillChange(notification: Notification){
         // keyboardWillEnter will be called by default. If other behavour is required override this method in your controller and do not call the super method.
         self.keyboardWillEnter(notification: notification)
     }
     
-    open func keyboardWillHide(keyboardEndFrame:CGRect, animationDuration: TimeInterval, animationCurve: Int){
+    open func keyboardWillHide(keyboardEndFrame: CGRect, animationDuration: TimeInterval, animationCurve: Int){
     }
     
-    open func keyboardWillHide(notification:NSNotification){
+    open func keyboardWillHide(notification: Notification){
     }
 }
