f35bf1d9e89e0117e045d0d0f50be17dc92a23f7
Rens Wijnmalen <r.wijnmalen@yipyip.nl>
2018-09-21 09:07:26 +0200

    Updated the way of handeling the keyboard changes

--- Pod/Classes/BaseClasses/YipYipViewControllerBase.swift
+++ Pod/Classes/BaseClasses/YipYipViewControllerBase.swift
@@ -11,7 +11,6 @@ import UIKit
 open class YipYipViewControllerBase: UIViewController {
     
     private var _keyboardIsShown: Bool = false
-    private var _lastKnownKeyboardHeight: CGFloat = 0
     
     // ----------------------------------------------------
     // MARK: - Computed properties
@@ -19,10 +18,6 @@ open class YipYipViewControllerBase: UIViewController {
     
     public var keyboardIsShown: Bool{
         return self._keyboardIsShown
-    }
-    
-    public var lastKnownKeyboardHeight: CGFloat{
-        return self._lastKnownKeyboardHeight
     }
     
     // ----------------------------------------------------
@@ -33,6 +28,7 @@ open class YipYipViewControllerBase: UIViewController {
         super.viewWillAppear(animated)
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillChangeFrame(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillEnterForground(_:)), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
@@ -41,6 +37,7 @@ open class YipYipViewControllerBase: UIViewController {
         super.viewDidDisappear(animated)
         
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
     }
@@ -49,52 +46,54 @@ open class YipYipViewControllerBase: UIViewController {
     // MARK: - Keyboard methods
     // ----------------------------------------------------
     
-    open func dismissKeyboard(){
-        self.view.endEditing(true)
+    // Notifications
+    
+    @objc open func keyboardWillShow(_ notification: Notification){
+        self.adjustForKeyboard(notification: notification)
     }
     
-    @objc private func keyboardWillShow(_ notification: Notification){
-        
-        if let keyboardEndFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-            
-            let keyboardHeightDidChange = self.lastKnownKeyboardHeight != keyboardEndFrame.height
-            let keyboardIsShown = self._keyboardIsShown
-            self._keyboardIsShown = true
-            self._lastKnownKeyboardHeight = keyboardEndFrame.height
-            
-            guard let keyboardAnimationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
-            guard let keyboardAnimationCurve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int else { return }
-            if keyboardIsShown{
-                if keyboardHeightDidChange{
-                    self.keyboardWillChange(keyboardEndFrame: keyboardEndFrame, animationDuration: keyboardAnimationDuration, animationCurve: keyboardAnimationCurve)
-                }
-            } else if keyboardHeightDidChange {
-                self.keyboardWillEnter(keyboardEndFrame: keyboardEndFrame, animationDuration: keyboardAnimationDuration, animationCurve: keyboardAnimationCurve)
-            }
-            
-            if keyboardIsShown{
-                if keyboardHeightDidChange{
-                    self.keyboardWillChange(notification: notification)
-                }
-            } else if keyboardHeightDidChange {
-                self.keyboardWillEnter(notification: notification)
-            }
-        }
+    @objc open func keyboardWillChangeFrame(_ notification: Notification){
+        self.adjustForKeyboard(notification: notification)
     }
     
-    @objc private func keyboardWillHide(_ notification: Notification){
-        self.keyboardWillHide(notification: notification)
-        
-        guard let keyboardEndFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-        guard let keyboardAnimationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
-        guard let keyboardAnimationCurve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int else { return }
-        self.keyboardWillHide(keyboardEndFrame: keyboardEndFrame, animationDuration: keyboardAnimationDuration, animationCurve: keyboardAnimationCurve)
-        self._keyboardIsShown = false
-        self._lastKnownKeyboardHeight = 0
+    @objc open func keyboardWillHide(_ notification: Notification){
+        self.adjustForKeyboard(notification: notification)
     }
     
     @objc private func applicationWillEnterForground(_ notification: Notification){
         self.dismissKeyboard()
+    }
+    
+    // Open methods
+    
+    open func adjustForKeyboard(notification: Notification){
+        
+        guard let keyboardBeginFrame = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect else { return }
+        guard let keyboardEndFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        guard let keyboardAnimationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        guard let keyboardAnimationCurve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int else { return }
+        
+        switch notification.name{
+        case UIResponder.keyboardWillShowNotification:
+            if !self._keyboardIsShown{
+                self._keyboardIsShown = true
+                self.keyboardWillEnter(notification: notification)
+                self.keyboardWillEnter(keyboardEndFrame: keyboardEndFrame, animationDuration: keyboardAnimationDuration, animationCurve: keyboardAnimationCurve)
+            }
+        case UIResponder.keyboardWillHideNotification:
+            if self._keyboardIsShown{
+                self._keyboardIsShown = false
+                self.keyboardWillHide(notification: notification)
+                self.keyboardWillHide(keyboardEndFrame: keyboardEndFrame, animationDuration: keyboardAnimationDuration, animationCurve: keyboardAnimationCurve)
+            }
+        case UIResponder.keyboardWillChangeFrameNotification:
+            if keyboardBeginFrame.origin.y != keyboardEndFrame.origin.y{
+                self.keyboardWillChange(notification: notification)
+                self.keyboardWillChange(keyboardEndFrame: keyboardEndFrame, animationDuration: keyboardAnimationDuration, animationCurve: keyboardAnimationCurve)
+            }
+        default:
+            break
+        }
     }
     
     open func keyboardWillEnter(keyboardEndFrame: CGRect, animationDuration: TimeInterval, animationCurve: Int){
@@ -104,18 +103,18 @@ open class YipYipViewControllerBase: UIViewController {
     }
     
     open func keyboardWillChange(keyboardEndFrame: CGRect, animationDuration: TimeInterval, animationCurve: Int){
-        // keyboardWillEnter will be called by default. If other behavour is required override this method in your controller and do not call the super method.
-        self.keyboardWillEnter(keyboardEndFrame: keyboardEndFrame, animationDuration: animationDuration, animationCurve: animationCurve)
     }
     
     open func keyboardWillChange(notification: Notification){
-        // keyboardWillEnter will be called by default. If other behavour is required override this method in your controller and do not call the super method.
-        self.keyboardWillEnter(notification: notification)
     }
     
     open func keyboardWillHide(keyboardEndFrame: CGRect, animationDuration: TimeInterval, animationCurve: Int){
     }
     
     open func keyboardWillHide(notification: Notification){
+    }
+    
+    open func dismissKeyboard(){
+        self.view.endEditing(true)
     }
 }
