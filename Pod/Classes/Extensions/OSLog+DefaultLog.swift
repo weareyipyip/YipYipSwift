4ceba3726f8cd48da252545dcb98090ee1a4ad22
Rens Wijnmalen <r.wijnmalen@yipyip.nl>
2019-01-28 11:34:38 +0100

    Updated the OSLog categories to public so they can be used in the project as well

--- Pod/Classes/Extensions/OSLog+DefaultLog.swift
+++ Pod/Classes/Extensions/OSLog+DefaultLog.swift
@@ -15,12 +15,12 @@ import os.log
 extension OSLog {
     private static var subsystem = Bundle.main.bundleIdentifier!
     
-    static let app = OSLog(subsystem: subsystem, category: "app") // Application layer
-    static let ux = OSLog(subsystem: subsystem, category: "ux") // User experiance layer
-    static let netwerk = OSLog(subsystem: subsystem, category: "netwerk") // Networking layer
-    static let parsing = OSLog(subsystem: subsystem, category: "parsing") // Parsing layer
-    static let database = OSLog(subsystem: subsystem, category: "database") // Database layer
+    public static let app = OSLog(subsystem: subsystem, category: "app") // Application layer
+    public static let ux = OSLog(subsystem: subsystem, category: "ux") // User experiance layer
+    public static let netwerk = OSLog(subsystem: subsystem, category: "netwerk") // Networking layer
+    public static let parsing = OSLog(subsystem: subsystem, category: "parsing") // Parsing layer
+    public static let database = OSLog(subsystem: subsystem, category: "database") // Database layer
     
-    static let userLoginFlow = OSLog(subsystem: subsystem, category: "userLoginFlow") // User login flow layer (Discuss)
-    static let analytics = OSLog(subsystem: subsystem, category: "analytics") // Analytics layer  (Discuss)
+    public static let userLoginFlow = OSLog(subsystem: subsystem, category: "userLoginFlow") // User login flow layer (Discuss)
+    public static let analytics = OSLog(subsystem: subsystem, category: "analytics") // Analytics layer  (Discuss)
 }
