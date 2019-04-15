7dc8cc8933ec3994d456c3997282ade4e415b185
Rens Wijnmalen <r.wijnmalen@yipyip.nl>
2019-04-15 15:55:36 +0200

    Organized example project

---
+++ Example/YipYipSwiftExample/Classes/Service/Services.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  YipYipSwiftExample
+//
+//  Created by Rens Wijnmalen on 15/04/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import YipYipSwift
+
+class Services: YipYipServicesBase{
+    
+    
+    
+}
--- Example/YipYipSwiftExample/Classes/ViewControllers/ViewController.swift
+++ Example/YipYipSwiftExample/Classes/ViewControllers/ViewController.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
+import YipYipSwift
 
-class ViewController: UIViewController {
+class ViewController: YipYipViewControllerBase {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
